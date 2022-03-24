78c64c514d3e3d200dda3ccf83e06d45d10974a0
woong <inwoong100@gmail.com>
2022-03-24 23:41:51 +0900

    3.24

--- jsLecture/js-100-master/case5_progress_bar/practice/script.js
+++ jsLecture/js-100-master/case5_progress_bar/practice/script.js
@@ -4,4 +4,29 @@
   const get = (target) => {
     return document.querySelector(target)
   }
+  let timerId
+  const $progressbar = get('.progress-bar')
+
+  const throttle = (callback, time) => {
+    if (timerId) return
+    timerId = setTimeout(() => {
+      callback()
+      timerId = undefined
+    }, time)
+  }
+
+  const onScroll = () => {
+    const height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight
+
+    const scrollTop = document.documentElement.scrollTop
+
+    const width = (scrollTop / height) * 100
+    $progressbar.style.width = width + '%'
+  }
+
+  window.addEventListener('scroll', () => {
+    throttle(onScroll, 50)
+  })
 })()
--- jsLecture/js-100-master/case6_infinite_scroll/practice/script.js
+++ jsLecture/js-100-master/case6_infinite_scroll/practice/script.js
@@ -4,4 +4,86 @@
   const get = (target) => {
     return document.querySelector(target)
   }
+
+  let page = 1
+  const limit = 10
+  const $posts = get('.posts')
+  const end = 100
+  let total = 10
+
+  const $loader = get('.loader')
+
+  const getPost = async () => {
+    const API_URL = `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=${limit}`
+    const response = await fetch(API_URL)
+    if (!response.ok) {
+      throw new Error('Error')
+    } else {
+      return await response.json()
+    }
+  }
+
+  const showPosts = (posts) => {
+    posts.forEach((post) => {
+      const $post = document.createElement('div')
+      $post.classList.add('post')
+      $post.innerHTML = `
+        <div class="header">
+          <div class="id">${post.id}</div>
+          <div class="title">${post.title}</div>
+        </div>
+        <div class="body">
+          ${post.body}
+        </div>
+      `
+      $posts.appendChild($post)
+    })
+  }
+
+  const loadPost = async () => {
+    showLoader()
+    try {
+      const response = await getPost()
+      showPosts(response)
+    } catch (error) {
+      console.error(error)
+    } finally {
+      hideLoader()
+    }
+  }
+
+  const showLoader = () => {
+    $loader.classList.add('show')
+  }
+
+  const hideLoader = () => {
+    $loader.classList.remove('show')
+  }
+
+  const onScroll = () => {
+    console.log('scroll')
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement
+    if (total === end) {
+      window.removeEventListener('scroll', onScroll)
+      return
+    }
+    if (scrollTop >= scrollHeight - clientHeight - 50) {
+      page++
+      total += 10
+      loadPost()
+    }
+  }
+  let timerId
+  const throttle = (callback, time) => {
+    if (timerId) return
+    timerId = setTimeout(() => {
+      callback()
+      timerId = undefined
+    }, time)
+  }
+
+  window.addEventListener('DOMContentLoaded', () => {
+    loadPost()
+    window.addEventListener('scroll', () => throttle(onScroll, 1000))
+  })
 })()
