7f983683df0ef895dc7abefeb0452c6bc7b3bea5
woong <inwoong100@gmail.com>
2022-04-01 21:25:19 +0900

    feature/js5 완성

--- Project_PART1/Javascript/Mission5/App.js
+++ Project_PART1/Javascript/Mission5/App.js
@@ -1,63 +1,103 @@
 // do something!
-const API_KEY = '16343e64273943aca7da7937bb5fb66f';
+const API_KEY = '551d3898eebf4c5a83e3565e18026423';
 const baseURL = 'https://newsapi.org/v2/top-headlines';
-let currentPage = 1;
+let $newsList;
+
 // const page = 1;
-const limit = 5;
 
-let data = [];
+const items = [
+  ['all', '전체보기'],
+  ['business', '비즈니스'],
+  ['entertainment', '엔터테인먼트'],
+  ['health', '건강'],
+  ['science', '과학'],
+  ['sports', '스포츠'],
+  ['technology', '기술'],
+];
 const get = (target) => document.querySelector(target);
+const $root = get('#root');
 
-const $newsList = get('.news-list');
-const $scrollObserver = get('.scroll-observer');
-
-let throttleCheck;
-const throttle = (callback) => {
-  return function () {
-    if (!throttleCheck) {
-      throttleCheck = setTimeout(() => {
-        callback(...arguments);
-        throttleCheck = false;
-      }, 500);
-    }
-  };
-};
-
-const getNews = async (page, pageSize = 5) => {
-  try {
-    return await axios.get(
-      `${baseURL}?country=kr&page=${page}&pageSize=${pageSize}&apiKey=${API_KEY}`
-    );
-  } catch (error) {
-    console.error(error);
+var state = { selected: '' };
+let proxy = new Proxy(
+  { selected: 'all' },
+  {
+    set: function (target, prop, value, receiver) {
+      for (let i = 0; i < items.length; i++) {
+        if (value == items[i][1]) {
+          target.selected = items[i][0];
+        }
+      }
+      const $newsListContainer = get('.news-list-container');
+      const $scrollObserver = get('.scroll-observer');
+      $root.removeChild($newsListContainer);
+      $root.removeChild($scrollObserver);
+      new NewsList($root, target.selected);
+      return true;
+    },
   }
-};
+);
 
-const loadNews = async (page, pageSize = 5) => {
-  console.log('load');
-  showLoader();
-  try {
-    const response = await getNews(page, pageSize);
-    showNews(response.data.articles);
-  } catch (error) {
-    console.error(error.message);
-  } finally {
-    hideLoader();
-  }
-};
+function Nav(target, items) {
+  const $nav = document.createElement('nav');
+  $nav.classList.add('category-list');
+  const $ul = document.createElement('ul');
 
-const showNews = (items) => {
-  items.forEach((item) => {
-    const newsItems = makeContent(item);
-    $newsList.appendChild(newsItems);
+  $ul.addEventListener('click', (e) => selectCategory(e));
+
+  const $items = items.map((item) => {
+    const $li = document.createElement('li');
+    $li.classList.add('category-item');
+    $li.setAttribute('id', item[0]);
+    $li.innerText = item[1];
+    $ul.appendChild($li);
+    return $li;
   });
-};
 
-const makeContent = (item) => {
-  const { title, url, urlToImage, description } = item;
-  const $newsItem = document.createElement('section');
-  $newsItem.classList.add('news-item');
-  $newsItem.innerHTML = `
+  $items[0].classList.add('active');
+  $nav.appendChild($ul);
+  target.appendChild($nav);
+
+  const selectCategory = (event) => {
+    if (event.target.tagName !== 'LI') return;
+    resetSelected();
+    const $li = event.target;
+    proxy.selected = event.target.innerText;
+
+    $li.classList.add('active');
+  };
+
+  const resetSelected = () => {
+    $items.forEach((item) => item.classList.remove('active'));
+  };
+}
+
+function NewsList(target, category = 'all') {
+  console.log(category);
+
+  let currentPage = 0;
+  const limit = 5;
+
+  const $newsListContainer = document.createElement('div');
+  $newsListContainer.classList.add('news-list-container');
+  const $scrollObserver = document.createElement('div');
+  $scrollObserver.classList.add('scroll-observer');
+  const $imgBall = document.createElement('img');
+  $imgBall.setAttribute('src', 'img/ball-triangle.svg');
+  $imgBall.setAttribute('alt', 'Loading...');
+  $scrollObserver.appendChild($imgBall);
+
+  const $newsList = document.createElement('article');
+  $newsList.classList.add('news-list');
+  $newsListContainer.appendChild($newsList);
+
+  target.appendChild($newsListContainer);
+  target.appendChild($scrollObserver);
+
+  const makeContent = (item) => {
+    const { title, url, urlToImage, description } = item;
+    const $newsItem = document.createElement('section');
+    $newsItem.classList.add('news-item');
+    $newsItem.innerHTML = `
   <div class="thumbnail">
   <a href="${url}" target="_blank" rel="noopener noreferrer">
     <img
@@ -75,43 +115,85 @@ const makeContent = (item) => {
     ${description}
   </p>
 </div>`;
-  return $newsItem;
-};
+    return $newsItem;
+  };
 
-const hideLoader = () => {
-  $scrollObserver.style.display = 'none';
-};
-const showLoader = () => {
-  $scrollObserver.style.display = 'block';
-};
+  const getNews = async (page, pageSize = 5, category = 'all') => {
+    const response = await axios.get(
+      `${baseURL}?country=kr&category=${
+        category === 'all' ? '' : category
+      }&page=${page}&pageSize=${pageSize}&apiKey=${API_KEY}`
+    );
+    return response;
+  };
 
-const handleScroll = () => {
-  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-  if (scrollTop + clientHeight >= scrollHeight - 5) {
-    currentPage++;
-    loadNews(currentPage, limit);
-  }
-};
-// const options = {
-//   root: null,
-//   rootMargin: '90% 0px 0px 0px',
-//   threshold: 0.25,
-// };
+  const showNews = (items) => {
+    items.forEach((item) => {
+      const newsItems = makeContent(item);
+      $newsList.appendChild(newsItems);
+    });
+  };
 
-const io = new IntersectionObserver(
-  () => {
-    currentPage++;
-    loadNews(currentPage, limit);
-  },
-  { threshold: 0.98 }
-);
+  const loadNews = async (page, pageSize = 5, category = 'all') => {
+    console.log('load');
+    showLoader();
+    try {
+      const response = await getNews(page, pageSize, category);
+      showNews(response.data.articles);
+    } catch (error) {
+      console.error(error.message);
+    } finally {
+      hideLoader();
+    }
+  };
 
-const observeLastItem = () => {
-  const $newsItems = document.querySelectorAll('.news-item');
-  io.observe($newsItems[$newsItems.length - 1]);
+  const hideLoader = () => {
+    $scrollObserver.style.visibility = 'hidden';
+  };
+  const showLoader = () => {
+    $scrollObserver.style.visibility = 'visible';
+  };
+
+  const throttle = (callback, limit = 1000) => {
+    let waiting = true;
+    return function () {
+      if (waiting) {
+        callback();
+        waiting = false;
+        setTimeout(() => {
+          waiting = true;
+        }, limit);
+      }
+    };
+  };
+
+  const createObserver = () => {
+    console.log('createObserve');
+    let observer;
+    let options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 1,
+    };
+    observer = new IntersectionObserver(
+      throttle(() => {
+        currentPage++;
+        loadNews(currentPage, limit, category);
+      }),
+      options
+    );
+
+    observer.observe($scrollObserver);
+  };
+  // loadNews(currentPage, limit, category);
+  setTimeout(() => createObserver(), 100);
+}
+
+const init = () => {
+  new Nav($root, items);
+  new NewsList($root);
 };
 
 window.addEventListener('DOMContentLoaded', async () => {
-  await loadNews(currentPage, limit);
-  observeLastItem();
+  init();
 });
