843f3db0658badbda6a9376f4de7880281db4a04
woong <inwoong100@gmail.com>
2022-06-05 09:40:20 +0900

    Add 6.5/원시형참조

--- lecture/react/example/my-app_CRA/src/components/Counter.js
+++ lecture/react/example/my-app_CRA/src/components/Counter.js
@@ -2,11 +2,34 @@ import React, { useState } from 'react';
 
 export default function Counter() {
   const [count, setCount] = useState(0);
+  const [show, setShow] = useState(true);
+  const operators = ['+', '-', '*'];
+  const [operator, setOperator] = useState(operators[0]);
+
   return (
     <div>
-      <button onClick={() => setCount(count + 1)}>+1</button>
+      <button
+        onClick={() => {
+          let result;
+          if (operator == '+') result = count + 1;
+          else if (operator == '*') result = count * 1;
+          else if (operator == '-') result = count - 1;
+          setCount(result);
+        }}
+      >
+        {operator}1
+      </button>
+      <button onClick={() => setShow(!show)}>Show and Hide</button>
+      <button
+        onClick={() => {
+          const idx = Math.floor(Math.random() * operators.length);
+          setOperator(operators[idx]);
+        }}
+      >
+        Change Operator
+      </button>
       <br />
-      Counter: {count}
+      {show && `Counter: ${count}`}
     </div>
   );
 }
