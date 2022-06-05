3c6e9fd9092c3c68963a4494756b9f37e01061d8
woong <inwoong100@gmail.com>
2022-06-05 10:39:48 +0900

    add 6.5/react/stateMore

--- lecture/react/example/my-app_CRA/src/App.js
+++ lecture/react/example/my-app_CRA/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+import Calculator from './components/Calculator';
 import Counter from './components/Counter';
+import Counter2 from './components/Counter2';
 import Header from './components/Header';
 import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div className='App'>
-      <Counter />
+      <Calculator />
     </div>
   );
 }
