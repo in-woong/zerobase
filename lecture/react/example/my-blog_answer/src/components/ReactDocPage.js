a70289f5ea88243d4dd3f8452b803c48a519397f
woong <inwoong100@gmail.com>
2022-06-14 21:24:17 +0900

    add 6.14/react_async/await_mission

--- lecture/react/example/my-blog_answer/src/components/ReactDocPage.js
+++ lecture/react/example/my-blog_answer/src/components/ReactDocPage.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router';
 
 export default function ReactDocPage() {
+  const [data, setData] = useState({});
   const params = useParams();
   const navigate = useNavigate();
-  
+  const isLoading = { title: 'isLoading', body: 'Wait for Seconds Please' };
+  useEffect(() => {
+    console.log('useEffect');
+    async function fetchData() {
+      const res = await fetch(
+        `https://jsonplaceholder.typicode.com/posts/${params.docId}`
+      );
+      const result = res.json();
+      return result;
+    }
+    setData(isLoading);
+    fetchData().then((res) => setData(res));
+  }, [params]);
+
   return (
     <>
       <button onClick={() => navigate('/')}>logo</button>
-      <div>ReactDocPage #{params.docId}</div>
+      <h3>{data.title}</h3>
+      <div>ReactDocPage #{data.body}</div>
     </>
   );
 }
--- lecture/react/example/my-blog_answer/src/components/ReactPage.js
+++ lecture/react/example/my-blog_answer/src/components/ReactPage.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 
 export default function ReactPage() {
-  const docs = [
-    { id: 1, title: 'React 공부를 시작하면서', date: '05/01/2021' },
-    { id: 2, title: 'CRA 없이 리액트 프로젝트 시작하기', date: '12/01/2021' },
-    {
-      id: 3,
-      title: '이제는 사용해보자 useMEmo & useCallback',
-      date: '31/01/2021',
-    },
-    { id: 4, title: 'React 프로젝트에 Prettier 적용하기', date: '15/02/2021' },
-    { id: 5, title: 'React의 setState', date: '03/03/2022' },
-  ];
+  const [docs, setDocs] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+      const result = await res.json();
+      setDocs(result);
+    }
+    fetchData();
+  }, []);
+  // const docs = [
+  //   { id: 1, title: 'React 공부를 시작하면서', date: '05/01/2021' },
+  //   { id: 2, title: 'CRA 없이 리액트 프로젝트 시작하기', date: '12/01/2021' },
+  //   {
+  //     id: 3,
+  //     title: '이제는 사용해보자 useMEmo & useCallback',
+  //     date: '31/01/2021',
+  //   },
+  //   { id: 4, title: 'React 프로젝트에 Prettier 적용하기', date: '15/02/2021' },
+  //   { id: 5, title: 'React의 setState', date: '03/03/2022' },
+  // ];
   return (
     <div>
       {docs.map((doc) => (
@@ -24,7 +33,7 @@ export default function ReactPage() {
           {doc.title}
         </Link>
       ))}
-      <Outlet />
+      <Outlet docs={docs} />
     </div>
   );
 }
