8a64f37b96ebabb343e7f477beb31f70ecbcc263
woong <inwoong100@gmail.com>
2022-06-18 15:05:12 +0900

    add 6.18/react_reactQuery

--- lecture/react/example/my-blog_answer/src/components/ReactPage.js
+++ lecture/react/example/my-blog_answer/src/components/ReactPage.js
@@ -1,22 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 import axios from 'axios';
+import useSWR from 'swr';
 
 export default function ReactPage() {
-  const [docs, setDocs] = useState([]);
-  useEffect(() => {
-    async function fetchData() {
-      const result = await axios.get(
-        'https://jsonplaceholder.typicode.com/posts'
-      );
+  const [number, setNumer] = useState(0);
+  async function fetcher(url) {
+    const result = await axios.get(url);
+    console.log(result.data);
+    return result.data;
+  }
+  const { data: docs, error } = useSWR('posts', () =>
+    fetcher('https://jsonplaceholder.typicode.com/posts')
+  );
 
-      return result.data;
-    }
-    fetchData().then((res) => setDocs(res));
-  }, []);
-
+  if (error) return <div>fail to load</div>;
+  if (!docs) return <div>loading...</div>;
   return (
     <div>
+      <button onClick={() => setNumer(number + 1)}>{number}</button>
       {docs.map((doc) => (
         <Link
           key={doc.id}
