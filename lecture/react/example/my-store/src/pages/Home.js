9a93ac34640e986558e3b8d21bd3592da262d545
woong <inwoong100@gmail.com>
2022-06-19 10:24:56 +0900

    add 6.19/react_reactQuery

---
+++ lecture/react/example/my-store/src/pages/Home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useQuery } from 'react-query';
+import { useNavigate } from 'react-router-dom';
+import { getUser } from '../mocks/api';
+
+export default function Home() {
+  const navigate = useNavigate();
+  const { data, isLoading } = useQuery('@getUser', getUser, {
+    staleTime: Infinity,
+  });
+
+  return (
+    <>
+      {isLoading ? (
+        <span>isLoading...</span>
+      ) : (
+        <div>
+          <h1>Hello, {data.nickName}</h1>
+          <button onClick={() => navigate('/edit')}>Go Edit Page</button>
+        </div>
+      )}
+    </>
+  );
+}
