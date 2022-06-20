0573bc30c717c7d74e0b781d9384d2158c6bbbc0
woong <inwoong100@gmail.com>
2022-06-20 23:09:02 +0900

    add 6.20/react_ssr

--- lecture/react/example/my-store/src/pages/Edit.js
+++ lecture/react/example/my-store/src/pages/Edit.js
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, lazy, Suspense } from 'react';
 import { useMutation, useQueryClient, useQueries } from 'react-query';
 import { getPosts, getUser, updateNickname } from '../mocks/api';
 
 // import ("./Math").then(math=>console.log(math(1,2)))
+
+const Post = lazy(() => import('./Post'));
 
 export default function Edit() {
   const [inputValue, setInputValue] = useState('');
@@ -16,7 +18,6 @@ export default function Edit() {
       staleTime: Infinity,
     },
   ]);
-  console.log(results);
 
   const user = results[0].data;
   const posts = results[1].data;
@@ -39,7 +40,7 @@ export default function Edit() {
   return (
     <>
       <h1>Edit</h1>
-      <h3>현재 닉네임: {user.nickName}</h3>
+      <h3>현재 닉네임: {user?.nickName}</h3>
       <form onSubmit={handleSubmit}>
         <label>
           변경할 닉네임:
@@ -48,7 +49,7 @@ export default function Edit() {
       </form>
       <ul>
         {posts?.map((post, idx) => (
-          <li key={idx}>{post.title}</li>
+          <Post title={post.title} />
         ))}
       </ul>
     </>
