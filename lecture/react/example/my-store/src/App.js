c493ac41adaadf730f5c946f0139a372e5a3f274
woong <inwoong100@gmail.com>
2022-06-20 22:24:49 +0900

    add 6.20/react_suspence

--- lecture/react/example/my-store/src/App.js
+++ lecture/react/example/my-store/src/App.js
@@ -2,18 +2,27 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Edit from './pages/Edit';
 import { QueryClientProvider, QueryClient } from 'react-query';
+import { Suspense } from 'react';
 
-const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+    },
+  },
+});
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/edit' element={<Edit />} />
-        </Routes>
-      </BrowserRouter>
+      <Suspense fallback={<span>isLoading...</span>}>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/edit' element={<Edit />} />
+          </Routes>
+        </BrowserRouter>
+      </Suspense>
     </QueryClientProvider>
   );
 }
