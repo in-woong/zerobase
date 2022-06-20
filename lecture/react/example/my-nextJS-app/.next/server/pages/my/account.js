3b9fbf2c83eb191962a38ad5571d9350c028959d
woong <inwoong100@gmail.com>
2022-06-20 23:32:56 +0900

    6.20 lecture/react_nextJS

---
+++ lecture/react/example/my-nextJS-app/.next/server/pages/my/account.js
@@ -0,0 +1,46 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/my/account";
+exports.ids = ["pages/my/account"];
+exports.modules = {
+
+/***/ "./pages/my/account.js":
+/*!*****************************!*\
+  !*** ./pages/my/account.js ***!
+  \*****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ Account)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n\nfunction Account() {\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"h2\", {\n        children: \"my account\"\n    }, void 0, false, {\n        fileName: \"/Users/woong/Documents/Zerobase/zerobase/lecture/react/example/my-nextJS-app/pages/my/account.js\",\n        lineNumber: 2,\n        columnNumber: 10\n    }, this);\n};\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9teS9hY2NvdW50LmpzLmpzIiwibWFwcGluZ3MiOiI7Ozs7OztBQUFBO0FBQWUsU0FBU0EsT0FBTyxHQUFHO0lBQ2hDLHFCQUFPLDhEQUFDQyxJQUFFO2tCQUFDLFlBQVU7Ozs7O1lBQUssQ0FBQztDQUM1QiIsInNvdXJjZXMiOlsid2VicGFjazovLy8uL3BhZ2VzL215L2FjY291bnQuanM/ZGI3OCJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCBmdW5jdGlvbiBBY2NvdW50KCkge1xuICByZXR1cm4gPGgyPm15IGFjY291bnQ8L2gyPjtcbn1cbiJdLCJuYW1lcyI6WyJBY2NvdW50IiwiaDIiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./pages/my/account.js\n");
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/my/account.js"));
+module.exports = __webpack_exports__;
+
+})();
