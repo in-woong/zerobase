9c27db22b059476a2b2c53cdd5d98177d4be89a3
woong <inwoong100@gmail.com>
2022-06-28 00:16:38 +0900

    add 6.27/typescript_todolist

--- lecture/typescript/project/vite-ts/src/main.ts
+++ lecture/typescript/project/vite-ts/src/main.ts
@@ -1,51 +1,59 @@
 import './style.css';
-
 const VALID_NUMBER_OF_DIGITS = 3;
 const BASE_DIGIT = 10;
-let temp;
-let calculator;
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
-const input = app.querySelector('.input');
-const numberBtn = app.querySelectorAll('.number');
-const calBtn = app.querySelectorAll('.cal');
-const returnBtn = app.querySelector('.return');
-const resetBtn = app.querySelector('.reset');
+function Calculator() {
+  let temp;
+  let calculator;
 
-const clickNumber = (e: any) => {
-  const inputValue = input.value;
-  console.log(inputValue);
-  input.value = e.target.innerText + inputValue;
-};
+  const input = app.querySelector('.input');
+  const numberBtn = app.querySelectorAll('.number');
+  const calBtn = app.querySelectorAll('.cal');
+  const returnBtn = app.querySelector('.return');
+  const resetBtn = app.querySelector('.reset');
 
-const clickCal = (e) => {
-  temp = input.value;
-  input.value = '';
-  calculator = e.target.innerText;
-};
+  const clickNumber = (e: any) => {
+    const inputValue = input.value;
+    console.log(inputValue);
+    input.value = e.target.innerText + inputValue;
+  };
 
-const clickReturn = () => {
-  if (calculator == '+') {
-    input.value = Number(temp) + Number(input.value);
-  } else if (calculator == '-') {
-    input.value = Number(temp) - Number(input.value);
-  } else if (calculator == '*') {
-    input.value = Number(temp) * Number(input.value);
-  } else if (calculator == '%') {
-    input.value = Number(temp) % Number(input.value);
-  } else {
-    return;
-  }
-};
+  const clickCal = (e) => {
+    temp = input.value;
+    input.value = '';
+    calculator = e.target.innerText;
+  };
 
-const clickReset = () => {
-  input.value = '';
-  temp = 0;
-  calculator = '';
-};
+  const clickReturn = () => {
+    if (calculator == '+') {
+      input.value = Number(temp) + Number(input.value);
+    } else if (calculator == '-') {
+      input.value = Number(temp) - Number(input.value);
+    } else if (calculator == '*') {
+      input.value = Number(temp) * Number(input.value);
+    } else if (calculator == '%') {
+      input.value = Number(temp) % Number(input.value);
+    } else {
+      return;
+    }
+  };
 
-numberBtn.forEach((btn) => btn.addEventListener('click', clickNumber));
-calBtn.forEach((btn) => btn.addEventListener('click', clickCal));
-returnBtn?.addEventListener('click', clickReturn);
-resetBtn?.addEventListener('click', clickReset);
+  const clickReset = () => {
+    input.value = '';
+    temp = 0;
+    calculator = '';
+  };
+
+  const initEvent = () => {
+    numberBtn.forEach((btn) => btn.addEventListener('click', clickNumber));
+    calBtn.forEach((btn) => btn.addEventListener('click', clickCal));
+    returnBtn?.addEventListener('click', clickReturn);
+    resetBtn?.addEventListener('click', clickReset);
+  };
+}
+
+const calculator = new Calculator();
+
+calculator.initEvent();
