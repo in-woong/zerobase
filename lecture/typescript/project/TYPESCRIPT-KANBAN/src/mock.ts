a102e794d469b2c9c253a2f86318b0e6273a0ceb
woong <inwoong100@gmail.com>
2022-07-03 12:21:14 +0900

    add 7.3/typescript_project_mock

---
+++ lecture/typescript/project/TYPESCRIPT-KANBAN/src/mock.ts
@@ -0,0 +1,37 @@
+import { Kanban, Todo, DoneTodo, inProgressTodo } from './type';
+
+export const todoList: Todo[] = [
+  {
+    id: 0,
+    content: { title: '제목', body: '내용' },
+    isDone: false,
+    category: 'to-do',
+    tags: [{ id: 0, content: '태그1' }],
+  },
+];
+
+export const inProgressList: inProgressTodo[] = [
+  {
+    id: 0,
+    content: { title: '제목', body: '내용' },
+    isDone: false,
+    category: 'in-progress',
+    tags: [{ id: 0, content: '태그1' }],
+  },
+];
+
+export const doneList: DoneTodo[] = [
+  {
+    id: 0,
+    content: { title: '제목', body: '내용' },
+    isDone: true,
+    category: 'done',
+    tags: [{ id: 0, content: '태그1' }],
+  },
+];
+
+export const defualtKanban: Kanban[] = [
+  { id: 0, title: 'To do', todos: todoList },
+  { id: 1, title: 'In Progress', todos: inProgressList },
+  { id: 2, title: 'Done', todos: doneList },
+];
