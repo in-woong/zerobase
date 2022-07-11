699b875bf5431c8013dd9ad25a0f3acd7fd3d475
woong <inwoong100@gmail.com>
2022-07-11 22:29:04 +0900

    feature/add Nav and Slider

--- project2/src/App.tsx
+++ project2/src/App.tsx
@@ -1,5 +1,21 @@
-function App() {
-  return <div className='App'></div>;
+import '../styles/global.css';
+import Footer from './components/Footer';
+import Nav from './components/Nav';
+import Slider from './components/Slider';
+import Index from './views';
+
+function App({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <div className='App'>
+        <Nav />
+        <Slider />
+        <Index />
+        <Footer />
+      </div>
+    </>
+  );
 }
 
 export default App;
