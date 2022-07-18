63b2dd0a680fd013feb927429f7dcb3aae16d753
woong <inwoong100@gmail.com>
2022-07-19 00:19:19 +0900

    hotfix 장바구니 Nav 숫자 동작활성화

--- react-shop/src/components/Nav.tsx
+++ react-shop/src/components/Nav.tsx
@@ -119,7 +119,7 @@ export default function Nav() {
                 ></path>
               </svg>
               <span className='inline-flex items-center justify-center absolute top-0 right-0 px-2 py-1 rounded-full bg-red-500 text-xs font-bold leading-none text-gray-200 transform translate-x-1/2 -translate-y-1/2'>
-                0
+                {count}
               </span>
             </span>
           </Link>
