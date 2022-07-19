70c9da590fcc4dfb32839cb4fef189799d9c7cdf
woong <inwoong100@gmail.com>
2022-07-19 23:54:42 +0900

    hotfix 검색 창 z-index 수정

--- react-shop/src/components/ProductsList.tsx
+++ react-shop/src/components/ProductsList.tsx
@@ -19,10 +19,10 @@ const ProductsList = ({
               to={`/product/${product.id}`}
               className='card card-bordered border-gray200'
             >
-              <figure className='z-1 flex h-80 bg-white overflow-hidden first-of-type:transition-transform first-of-type:duration-300 hover:first-of-type:-translate-y-1 hover:first-of-type:scale-125 '>
+              <figure className='-z-10 flex h-80 bg-white overflow-hidden first-of-type:transition-transform first-of-type:duration-300 hover:first-of-type:-translate-y-1 hover:first-of-type:scale-125 '>
                 <img src={product.image} alt='상품 이미지' className='' />
               </figure>
-              <div className=' z-10 card-body bg-gray-200 dark:bg-gray-700 text-gray-900 dark:text-gray-200'>
+              <div className='card-body bg-gray-200 dark:bg-gray-700 text-gray-900 dark:text-gray-200'>
                 <p className='card-title text-base'>{product.title}</p>
                 <p className='text-base'>{product.price}</p>
               </div>
